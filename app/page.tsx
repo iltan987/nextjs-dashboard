4b6367d32325ab7807c9db07d8bd11980fbabe5f
iltan987 <22109906@student.ciu.edu.tr>
2025-01-15 14:04:56 +0300

    formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+import AcmeLogo from "@/app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 import { lusitana } from "./ui/fonts";
 import Image from "next/image";
 
@@ -14,9 +14,9 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
           <p
-className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
->
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
